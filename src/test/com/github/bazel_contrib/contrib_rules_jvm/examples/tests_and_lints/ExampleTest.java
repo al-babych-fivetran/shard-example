35aab4c76d2da36ce736b776cd31449e3d924e45
Aleksei Babich <alexey.babych@fivetran.com>
2024-06-07 09:43:38 +0200

    remove extra

--- src/test/com/github/bazel_contrib/contrib_rules_jvm/examples/tests_and_lints/ExampleTest.java
+++ src/test/com/github/bazel_contrib/contrib_rules_jvm/examples/tests_and_lints/ExampleTest.java
@@ -13,12 +13,4 @@ public class ExampleTest {
     System.out.println("Test: " + value + ", Shard: " + shard);
     TimeUnit.SECONDS.sleep(1);
   }
-
-  @ParameterizedTest
-  @ValueSource(ints = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20})
-  public void passes1(int value) throws InterruptedException {
-    var shard = System.getenv("TEST_SHARD_INDEX");
-    System.out.println("Test1: " + value + ", Shard: " + shard);
-    TimeUnit.SECONDS.sleep(1);
-  }
 }
